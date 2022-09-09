652a155194c861cd3a2626186db09b36e246255d
wangz <wangz@DESKTOP-O4BJMKK>
2022-09-09 20:44:52 +0800

    添加项目文件。

---
+++ defaultproj/defaultproj.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <tuple>
+#include <ctime>
+#include <stdio.h>
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    int a;
+    std::ignore=scanf("%d", &a);
+    return 0;
+}
